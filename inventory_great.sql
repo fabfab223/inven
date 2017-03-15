1867caec42032e734e1511e93f161e45083f812b
fabiyanu ramadani <fabiyanu ramadani>
2017-03-15 14:18:05 +0700

    Database Baru

--- inventory_great.sql
+++ inventory_great.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2017 at 02:23 AM
+-- Generation Time: Mar 15, 2017 at 08:16 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.3
 
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `uname`, `pass`, `foto`) VALUES
-(8, 'admin', '0a87761f44b83488defc5a04e709ac48', 'kallyas_icon.png');
+(2, 'admin2', 'ccb2eed4963cf31376e9cde7354971b6', 'kallyas_icon.png'),
+(8, 'admin1', 'e10adc3949ba59abbe56e057f20f883e', 'kallyas_icon.png');
 
 -- --------------------------------------------------------
 
@@ -51,6 +52,16 @@ CREATE TABLE `barang` (
   `nama` text NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama`, `jumlah`) VALUES
+(6, 'Bulpoin', 24),
+(8, 'Pensil', 30),
+(9, 'Stempel', 8),
+(10, 'Flashdisk', 10);
 
 -- --------------------------------------------------------
 
@@ -78,6 +89,44 @@ CREATE TABLE `barang_masuk` (
   `nama_barang` text NOT NULL,
   `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id`, `tanggal`, `nama_barang`, `jumlah`) VALUES
+(6, '2017-03-06', 'Bulpoin', 5),
+(7, '2017-03-06', 'Bulpoin', 2),
+(8, '2017-03-07', 'Bulpoin', 2),
+(9, '2017-03-09', 'Petelot', 2),
+(10, '2020-03-15', 'Bulpoin', 2),
+(11, '2017-03-16', 'Bulpoin', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_pinjam`
+--
+
+CREATE TABLE `barang_pinjam` (
+  `id` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `nama_barang` text NOT NULL,
+  `nama_peminta` text NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL,
+  `tanggal_kembali` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_pinjam`
+--
+
+INSERT INTO `barang_pinjam` (`id`, `tanggal`, `tgl_kembali`, `nama_barang`, `nama_peminta`, `jumlah`, `id_status`, `tanggal_kembali`) VALUES
+(10, '2017-03-14 08:42:00', '2017-03-13', 'Bulpoin', 'Fabiyanu', 3, 2, '2017-03-16 11:39:00'),
+(12, '2017-03-13 12:13:00', '2017-03-15', 'Stempel', 'Fabiyan', 1, 2, '2017-03-14 12:13:00'),
+(13, '2017-03-15 08:35:00', '0000-00-00', 'Stempel', 'Aaron', 2, 1, '2017-03-16 08:35:00');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +157,12 @@ ALTER TABLE `barang_masuk`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `barang_pinjam`
+--
+ALTER TABLE `barang_pinjam`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -120,17 +175,22 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `barang_pinjam`
+--
+ALTER TABLE `barang_pinjam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
